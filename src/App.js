65415caca9e3e0033a62158db140999574a2e22e
dazariani <dato_davit@mail.ru>
2023-07-15 13:30:01 +0400

    last state removed

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ function App() {
   const [appActive, setAppActive] = useState(false);
   const [cantBeZero, setCantBeZero] = useState(false);
 
-  const [sum, setSum] = useState(false);
-
   // Update UI
   useEffect(() => {
     if (!appActive) {
